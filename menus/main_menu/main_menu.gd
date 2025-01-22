d80722011c91590f5036a581b6c7cdb7ee6f0951
cjack-chmod <75388979+cjack-chmod@users.noreply.github.com>
2025-01-22 18:28:15 +0100

    escape button to close main menu menus

--- menus/main_menu/main_menu.gd
+++ menus/main_menu/main_menu.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 const OPTIONS_MENU: PackedScene = preload("res://menus/options_menu/options_menu.tscn")
 const CREDITS: PackedScene = preload("res://menus/credits/credits.tscn")
 
+@onready var current_focus: Node = self
 @onready var margin_container: MarginContainer = $MarginContainer
 
 
@@ -13,34 +14,43 @@ func _ready() -> void:
 	$%CreditsButton.pressed.connect(_on_credits_pressed)
 
 
+# launces main scene defined in ScreenTransition
 func _on_play_pressed() -> void:
 	# set any starting conditions here
 	ScreenTransition.transition_to_scene(ScreenTransition.main_scene_file_path)
 
 
+# launches credits menu, and binds back key to close fnc
 func _on_credits_pressed() -> void:
 	margin_container.visible = false
 	var credits_instance: CanvasLayer = CREDITS.instantiate()
 	add_child(credits_instance)
-	credits_instance.sig_back_pressed.connect(_on_credits_closed.bind(credits_instance))
+	current_focus = credits_instance
+	credits_instance.sig_back_pressed.connect(_on_secondary_menu_closed.bind(credits_instance))
 
 
+# launches options menu, and binds back key to close fnc
 func _on_options_pressed() -> void:
 	margin_container.visible = false
 	var options_instance: CanvasLayer = OPTIONS_MENU.instantiate()
 	add_child(options_instance)
-	options_instance.sig_back_pressed.connect(_on_options_closed.bind(options_instance))
+	current_focus = options_instance
+	options_instance.sig_back_pressed.connect(_on_secondary_menu_closed.bind(options_instance))
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 
-func _on_options_closed(options_instance: Node) -> void:
-	options_instance.queue_free()
+# closes the passed in secondary menu
+func _on_secondary_menu_closed(menu_instance: Node) -> void:
+	current_focus = self
+	menu_instance.queue_free()
 	margin_container.visible = true
 
 
-func _on_credits_closed(credits_instance: Node) -> void:
-	credits_instance.queue_free()
-	margin_container.visible = true
+# closes secondary menus using escape key
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if current_focus != self:
+			_on_secondary_menu_closed(current_focus)
