c8a53119b735c1f6b4e6f4e1db5c0104d3bc85ff
Cal <callummjack@gmail.com>
2025-04-10 10:45:08 +0200

    menu refactor

--- scenes/ui/menus/main_menu/main_menu.gd
+++ scenes/ui/menus/main_menu/main_menu.gd
@@ -1,17 +1,19 @@
-extends CanvasLayer
-
-const OPTIONS_MENU: PackedScene = preload("res://scenes/ui/menus/options_menu/options_menu.tscn")
-const CREDITS: PackedScene = preload("res://scenes/ui/menus/credits/credits.tscn")
-
-@onready var current_focus: Node = self
-@onready var container: Container = $MarginContainer
+extends TopLevelMenu
 
 
 func _ready() -> void:
+	super()
+
+	# connecting buttons
 	%PlayButton.pressed.connect(_on_play_pressed)
 	%OptionsButton.pressed.connect(_on_options_pressed)
 	%QuitButton.pressed.connect(_on_quit_pressed)
 	%CreditsButton.pressed.connect(_on_credits_pressed)
+
+
+##############################
+# Button Press Functions
+##############################
 
 
 # launces main scene defined in ScreenTransition
@@ -23,7 +25,8 @@ func _on_play_pressed() -> void:
 # launches credits menu, and binds back key to close fnc
 func _on_credits_pressed() -> void:
 	container.visible = false
-	var credits_instance: CanvasLayer = CREDITS.instantiate()
+	var credits_scene: PackedScene = load(CREDITS_SCENE_PATH)
+	var credits_instance: CanvasLayer = credits_scene.instantiate()
 	add_child(credits_instance)
 	current_focus = credits_instance
 	credits_instance.sig_back_pressed.connect(_close_secondary_menu.bind(credits_instance))
@@ -40,26 +43,3 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
-
-# closes the passed in secondary menu
-func _close_secondary_menu(menu_instance: Node) -> void:
-	current_focus = self
-	menu_instance.queue_free()
-	container.visible = true
-
-
-# closes secondary menus using escape key
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
-		# if current focus is secondary menu
-		if current_focus != self:
-			# if is options menu
-			if "is_control_menu_open" in current_focus:
-				# closing controls menu in options if open
-				if current_focus.is_control_menu_open:
-					current_focus.on_controls_closed(current_focus.controls_instance)
-					return
-
-			# else close secondary menu
-			_close_secondary_menu(current_focus)
